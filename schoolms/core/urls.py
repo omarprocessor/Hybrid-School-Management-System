47d79e559124b35421852687b2e320e73d38fd91
Omar Mohumed <wadaniinarais1@gmail.com>
2025-06-29 22:14:56 +0300

    added RetrieveUpdateDestroyAPIView for classRoom model

--- schoolms/core/urls.py
+++ schoolms/core/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # ClassRoom
     path('classrooms/', views.ClassRoomListCreateView.as_view(), name='classroom-list'),
-
+    path('classrooms/<int:id>/', views.ClassRoomDetailView.as_view(), name='classroom-detail'),
+   
 ]
--- schoolms/core/views.py
+++ schoolms/core/views.py
@@ -1,10 +1,18 @@
 from rest_framework import generics
 from .models import ClassRoom
 
-from .serializers import ClassRoomSerializer
+from .serializers import ClassRoomSerializer, SubjectSerializer
+from .models import Subject
 
 # Classroom views
 # This view handles both listing all classrooms and creating a new classroom.
 class ClassRoomListCreateView(generics.ListCreateAPIView):
     queryset = ClassRoom.objects.all()
     serializer_class = ClassRoomSerializer
+
+# This view handles retrieving, updating, and deleting a specific classroom by its ID.
+class ClassRoomDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ClassRoom.objects.all()
+    serializer_class = ClassRoomSerializer
+    lookup_field = 'id'  # Use 'id' as the lookup field for retrieving specific classrooms
+
