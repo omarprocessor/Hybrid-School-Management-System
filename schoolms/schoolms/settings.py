79d42de0101fcc84f64d9a5156fe1f6a43e13ba4
Omar Mohumed <wadaniinarais1@gmail.com>
2025-07-07 22:54:51 +0300

    Enable CORS in Django backend, refactor frontend API call to use environment variable, and update frontend README with .env setup instructions

--- schoolms/schoolms/settings.py
+++ schoolms/schoolms/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,4 +129,9 @@ AFRICASTALKING_USERNAME = ''
 AFRICASTALKING_API_KEY = ''
 
 TIME_ZONE = 'Africa/Nairobi'
-USE_TZ = True  
+USE_TZ = True
+
+# CORS settings for React frontend
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]  
